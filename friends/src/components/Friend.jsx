76cdc19ffbf4ee377dd81dd6e717deec5ea4a157
Hoxtygen <hoxtygen@live.com>
2019-06-13 16:05:54 +0100

    Add more property to dummy data
    To expand the properties of the data two more properties were added.

--- friends/src/components/Friend.jsx
+++ friends/src/components/Friend.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import styled from "styled-components";
 
 const Friend = ({friend, handleDelete, id}) => {
-  const { name, age, email} = friend;
+  const { name, age, email, location, category} = friend;
     return (
     <SingleFriend>
       <h3>Name: {name}</h3>
       <h4>Email: {email}</h4>
       <h4>Age: {age}</h4>
+      <h4>Location: {location}</h4>
+      <h4>Category: {category}</h4>
       <button onClick = {() => handleDelete(id)}>Delete</button>
       <button>Edit</button>
     </SingleFriend>
